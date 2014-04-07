4625e3ba31f81ee8275e7454d6fdd950223c113e
Rafael de Lucena Valle <rafaeldelucena@gmail.com>
2014-04-07 19:47:06 -0300

    Adding reference and install dependencies

--- mp42mp3.sh
+++ mp42mp3.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#from http://askubuntu.com/questions/174287/how-do-i-convert-an-mp4-to-an-mp3
+
+sudo apt-get insfall ffmpeg
+
 for f in $1*.mp4
 do
     name=`echo "$f" | sed -e "s/.mp4$//g"`
